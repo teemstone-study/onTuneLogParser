fc7196587677cf3d9ac26d974b8abb3c6575a68a
amanokaze <oniamano@gmail.com>
2023-03-20 10:24:10 +0900

    Windows Event Thread 생성 및 분리

--- main.py
+++ main.py
@@ -1,129 +1,31 @@
-import win32evtlog as wevt
-import win32event
-import datetime
-import locale
+import platform
 import threading
 import os 
 from dotenv import load_dotenv
 from modules.watchdog_handler import logCheck
-from modules.drain_handler import DrainHandler
-
-# Global variables
-server = 'localhost'
-logtype = 'System'
-file_fullpath = os.path.dirname(os.path.abspath(__file__))
-filename = file_fullpath + "\\eventlog.txt"
-
-def init_make_eventlog_file():
-    #current_locale = os.popen('systeminfo | findstr /B /C:"System Locale"').read()
-    #current_locale = locale.getlocale(locale.LC_CTYPE)
-
-    today = datetime.datetime.now().date()
-    day_ago = today - datetime.timedelta(days=1)
-
-    hand = wevt.OpenEventLog(server,logtype)
-    flags = wevt.EVENTLOG_FORWARDS_READ|wevt.EVENTLOG_SEQUENTIAL_READ
-    #total = wevt.GetNumberOfEventLogRecords(hand)
-    #parent_directory = os.path.dirname(file_fullpath)
-
-    while True:
-        events = wevt.ReadEventLog(hand, flags, 0)
-        if len(events) == 0:
-            break
-        if events:
-            f = open(filename, 'a', encoding='UTF8')
-            for evt in events:
-                if str(evt.TimeGenerated)[:10] == str(today):
-                    # print('Event Category:', evt.EventCategory)
-                    # print('Time Generated:', evt.TimeGenerated)
-                    # print('Source Name:', evt.SourceName)
-                    # print('Event ID:', idResult)
-                    # print('Event Type:', evt.EventType) 
-                    # 1 : 오류 2: 경고 4 : 정보
-
-                    idResult = evt.EventID & 0x0000FFFF
-                    wevt.EVENTLOG_ERROR_TYPE
-                    data = evt.StringInserts
-                    eventdata = ""
-                    if data != None:
-                        for msg in data:
-                            eventdata += msg
-                            
-                    logstring = f"[{evt.TimeGenerated}] [{evt.EventCategory}][{evt.SourceName}] [{idResult}] [{evt.EventType}]  [{eventdata}]"  
-                    f.write(logstring + "\n")
-                        
-                    # if data:
-                    #     print('Event Data:')
-                    #     for msg in data:
-                    #         print(msg)
-                    # print('*' * 100)
-                elif str(evt.TimeGenerated)[:10] == str(day_ago):
-                    break
-            f.close()
-
-    flags = wevt.EVENTLOG_BACKWARDS_READ|wevt.EVENTLOG_SEQUENTIAL_READ
-    return wevt.GetNumberOfEventLogRecords(hand)
-
-def refresh_eventlog(inittotal, drain_handler):
-    oldtotal = inittotal
-    while True:
-        hand = wevt.OpenEventLog(server,logtype)
-        flags = wevt.EVENTLOG_BACKWARDS_READ|wevt.EVENTLOG_SEQUENTIAL_READ
-        newtotal = wevt.GetNumberOfEventLogRecords(hand)
-
-        if oldtotal != newtotal:
-            today = datetime.datetime.now().date()
-            day_ago = today - datetime.timedelta(days=1)
-
-            events = wevt.ReadEventLog(hand, flags, 0, newtotal - oldtotal)
-            if events:
-                filename = file_fullpath + "\\eventlog.txt"
-                f = open(filename, 'a', encoding='UTF8')
-                for evt in events:
-                    if str(evt.TimeGenerated)[:10] == str(today):
-                        # print('Event Category:', evt.EventCategory)
-                        # print('Time Generated:', evt.TimeGenerated)
-                        # print('Source Name:', evt.SourceName)
-                        # print('Event ID:', idResult)
-                        # print('Event Type:', evt.EventType) 
-                        # 1 : 오류 2: 경고 4 : 정보
-                        idResult = evt.EventID & 0x0000FFFF
-                        wevt.EVENTLOG_ERROR_TYPE
-                        data = evt.StringInserts
-                        eventdata = ""
-                        for msg in data:
-                                eventdata += msg
-                            
-                        logstring = f"[{evt.TimeGenerated}] [{evt.EventCategory}][{evt.SourceName}] [{idResult}] [{evt.EventType}]  [{eventdata}]"  
-                            
-                        f.write(logstring + "\n")
-                        
-                        drain_handler.handle(logstring)
-                        # if data:
-                        #     print('Event Data:')
-                        #     for msg in data:
-                        #         print(msg)
-                        # print('*' * 100)
-                    elif str(evt.TimeGenerated)[:10] == str(day_ago):
-                        break
-                f.close()
-
-                drain_handler.report()
-
-                oldtotal += len(events)
+from modules.windows_event_handler import windows_event_log_check
 
 def main():
     load_dotenv()
     logPath = os.environ.get('CheckPath')
+    viewerPath = os.environ.get('ViewerPath')
+    managerPath = os.environ.get('ManagerPath')
 
-    filecheckThread = threading.Thread(target=logCheck, args=(logPath,))
-    filecheckThread.start()
+    if logPath:
+        filecheckThread = threading.Thread(target=logCheck, args=(logPath,))
+        filecheckThread.start()
 
-    init_total = init_make_eventlog_file()
-    drain_handler = DrainHandler(filename)
+    if viewerPath:
+        viewerCheckThread = threading.Thread(target=logCheck, args=(logPath,))
+        viewerCheckThread.start()
 
-    refresh_eventlog(init_total, drain_handler)
+    if managerPath:
+        managerCheckThread = threading.Thread(target=logCheck, args=(logPath,))
+        managerCheckThread.start()
 
+    if platform.system() == 'Windows':
+        winEventLogThread = threading.Thread(target=windows_event_log_check)
+        winEventLogThread.start()
 
 if __name__ == "__main__":
     main()
---
+++ modules/windows_event_handler.py
@@ -0,0 +1,113 @@
+import os
+import win32evtlog as wevt
+import datetime
+from modules.drain_handler import DrainHandler
+
+class WindowsEventHandler():
+    def __init__(self):
+        self.server = 'localhost'
+        self.logtype = 'System'
+
+        file_fullpath = os.path.dirname(os.path.abspath(__file__))
+        self.filename = file_fullpath + "\\..\\windows_event_log\\eventlog.txt"
+        #current_locale = os.popen('systeminfo | findstr /B /C:"System Locale"').read()
+        #current_locale = locale.getlocale(locale.LC_CTYPE)
+        #total = wevt.GetNumberOfEventLogRecords(hand)
+        #parent_directory = os.path.dirname(file_fullpath)
+
+        self.init_get_event()
+
+    def init_get_event(self):
+        today = datetime.datetime.now().date()
+        day_ago = today - datetime.timedelta(days=1)
+        hand = wevt.OpenEventLog(self.server,self.logtype)
+        flags = wevt.EVENTLOG_FORWARDS_READ|wevt.EVENTLOG_SEQUENTIAL_READ
+
+        while True:
+            events = wevt.ReadEventLog(hand, flags, 0)
+            if len(events) == 0:
+                break
+            if events:
+                f = open(self.filename, 'a', encoding='UTF8')
+                for evt in events:
+                    if str(evt.TimeGenerated)[:10] == str(today):
+                        # print('Event Category:', evt.EventCategory)
+                        # print('Time Generated:', evt.TimeGenerated)
+                        # print('Source Name:', evt.SourceName)
+                        # print('Event ID:', idResult)
+                        # print('Event Type:', evt.EventType) 
+                        # 1 : 오류 2: 경고 4 : 정보
+
+                        idResult = evt.EventID & 0x0000FFFF
+                        wevt.EVENTLOG_ERROR_TYPE
+                        data = evt.StringInserts
+                        eventdata = ""
+                        if data != None:
+                            for msg in data:
+                                eventdata += msg
+                                
+                        logstring = f"[{evt.TimeGenerated}] [{evt.EventCategory}][{evt.SourceName}] [{idResult}] [{evt.EventType}]  [{eventdata}]"  
+                        f.write(logstring + "\n")
+                            
+                        # if data:
+                        #     print('Event Data:')
+                        #     for msg in data:
+                        #         print(msg)
+                        # print('*' * 100)
+                    elif str(evt.TimeGenerated)[:10] == str(day_ago):
+                        break
+                f.close()
+
+        flags = wevt.EVENTLOG_BACKWARDS_READ|wevt.EVENTLOG_SEQUENTIAL_READ
+        self.current_total = wevt.GetNumberOfEventLogRecords(hand)
+        self.drain_handler = DrainHandler(self.filename)
+
+    def run(self):
+        while True:
+            hand = wevt.OpenEventLog(self.server,self.logtype)
+            flags = wevt.EVENTLOG_BACKWARDS_READ|wevt.EVENTLOG_SEQUENTIAL_READ
+            newtotal = wevt.GetNumberOfEventLogRecords(hand)
+
+            if self.current_total != newtotal:
+                today = datetime.datetime.now().date()
+                day_ago = today - datetime.timedelta(days=1)
+
+                events = wevt.ReadEventLog(hand, flags, 0, newtotal - self.current_total)
+                if events:
+                    f = open(self.filename, 'a', encoding='UTF8')
+                    for evt in events:
+                        if str(evt.TimeGenerated)[:10] == str(today):
+                            # print('Event Category:', evt.EventCategory)
+                            # print('Time Generated:', evt.TimeGenerated)
+                            # print('Source Name:', evt.SourceName)
+                            # print('Event ID:', idResult)
+                            # print('Event Type:', evt.EventType) 
+                            # 1 : 오류 2: 경고 4 : 정보
+                            idResult = evt.EventID & 0x0000FFFF
+                            wevt.EVENTLOG_ERROR_TYPE
+                            data = evt.StringInserts
+                            eventdata = ""
+                            for msg in data:
+                                    eventdata += msg
+                                
+                            logstring = f"[{evt.TimeGenerated}] [{evt.EventCategory}][{evt.SourceName}] [{idResult}] [{evt.EventType}]  [{eventdata}]"  
+                                
+                            f.write(logstring + "\n")
+                            
+                            self.drain_handler.handle(logstring)
+                            # if data:
+                            #     print('Event Data:')
+                            #     for msg in data:
+                            #         print(msg)
+                            # print('*' * 100)
+                        elif str(evt.TimeGenerated)[:10] == str(day_ago):
+                            break
+                    f.close()
+
+                    self.drain_handler.report()
+
+                    self.current_total += len(events)
+    
+def windows_event_log_check():
+    myWatcher = WindowsEventHandler()
+    myWatcher.run()
